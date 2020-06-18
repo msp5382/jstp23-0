1cefbab1c7fd463ed98b7afc6537b02f6b6f1cd4
matcha <meen.sankaew@gmail.com>
2020-06-18 16:37:32 +0700

    react-router5 test

---
+++ frontend/src/service/router/index.js
@@ -0,0 +1,16 @@
+import createRouter from "router5";
+import loggerPlugin from "router5-plugin-logger";
+import browserPlugin from "router5-plugin-browser";
+import routes from "./routes.js";
+
+export default function configureRouter() {
+  const router = createRouter(routes, {
+    defaultRoute: "inbox",
+  });
+
+  router.usePlugin(loggerPlugin);
+
+  router.usePlugin(browserPlugin());
+
+  return router;
+}
