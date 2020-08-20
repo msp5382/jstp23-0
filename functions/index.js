174ed79c0fc93c526ae1f4d0d20e724dc590927c
matcha <meen.sankaew@gmail.com>
2020-08-20 18:09:53 +0700

    debug

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,4 @@
-exports.test = (req, res) => res.send(req.body || "wowowow");
-
-exports.run = async (req, res) => {
+exports["jstp-23-backend"] = async (req, res) => {
   const resu = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   res.send(await resu.json());
 };
