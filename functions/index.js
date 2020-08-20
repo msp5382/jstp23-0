c174f1204cd344f151b4395d66aa154fe9001bc5
matcha <meen.sankaew@gmail.com>
2020-08-20 18:04:50 +0700

    test es6

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,6 @@
-export const test = (req, res) => res.send(req.body || "wowowow");
+exports.test = (req, res) => res.send(req.body || "wowowow");
+
+exports.run = async (req, res) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  res.send(await res.json());
+};
