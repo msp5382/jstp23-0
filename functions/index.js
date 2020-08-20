8ba7f631ebdc335306918c60cd8268a3be28869f
matcha <meen.sankaew@gmail.com>
2020-08-20 18:06:23 +0700

    debug

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 exports.test = (req, res) => res.send(req.body || "wowowow");
 
 exports.run = async (req, res) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  res.send(await res.json());
+  const resu = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  res.send(await resu.json());
 };
