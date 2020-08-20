5c40febbb68da22ce2919aad9c1c811a32e38df8
matcha <meen.sankaew@gmail.com>
2020-08-20 18:20:44 +0700

    debug

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
+const axios = require("axios");
+
 exports["jstp-23-backend"] = async (req, res) => {
-  const resu = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  res.send(await resu.json());
+  const resu = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  res.send(await resu.data);
 };
