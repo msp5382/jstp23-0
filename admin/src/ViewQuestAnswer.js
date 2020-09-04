fbfc2758603a1714c3ccea1b81af5f502cc43f17
matcha <meen.sankaew@gmail.com>
2020-09-04 14:59:15 +0700

    Log inแล้วค้าง(แบบในvidเลย)

--- admin/src/ViewQuestAnswer.js
+++ admin/src/ViewQuestAnswer.js
@@ -1,5 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { fetchQuestAnswer } from "./service/fetchQuestAnswer";
+import { fetchQuestByIdAndTime } from "./service/fetchQuests";
+const QuestAnswerR = (props) => {
+  const [QD, setQ] = useState({ quest_answer_id: "0", answerText: "" });
+  useEffect(() => {
+    fetchQuestByIdAndTime(props.q.quest_answerFor, props.q.time).then((res) =>
+      setQ(res)
+    );
+  }, [props.q]);
+  return (
+    <div class="border-solid border border-white	p-2 m-2 rounded flex">
+      <div class="w-1/2">
+        <div className=" text-sm text-gray-500">{props.q.quest_answer_id}</div>
+        <div className=" text-sm ">{props.q.answerText}</div>
+        {props.q.attatchFile.map((a) => (
+          <img src={a} alt="attatchfile" />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default (props) => {
   const [questAnswer, setQuestAnswer] = useState([]);
   const [answer, setAnswer] = useState([]);
@@ -14,26 +34,8 @@ export default (props) => {
   return (
     <>
       <div class="container mx-auto pt-5 ">
-        {questAnswer?.map((q) => (
-          <div class="border-solid border border-white	p-2 m-2 rounded flex">
-            <div class="w-1/2">
-              <div className=" text-sm text-gray-500">{q.answer_id}</div>
-              <div className=" text-sm ">{q.answerText}</div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div class="container mx-auto pt-5 ">
-        {answer?.map((q) => (
-          <div class="border-solid border border-white	p-2 m-2 rounded flex">
-            <div class="w-1/2">
-              <div className=" text-sm text-gray-500">{q.quest_answer_id}</div>
-              <div className=" text-sm ">{q.answerText}</div>
-              {q.attatchFile.map((a) => (
-                <img src={a} alt="attatchfile" />
-              ))}
-            </div>
-          </div>
+        {answer?.map((d) => (
+          <QuestAnswerR q={d} />
         ))}
       </div>
     </>
