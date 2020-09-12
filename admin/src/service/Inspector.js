013bcfc76f355ca9890f0dddd7b83da9b4ce5633
matcha <meen.sankaew@gmail.com>
2020-09-12 10:26:44 +0700

    added cleanAnswer func

--- admin/src/service/Inspector.js
+++ admin/src/service/Inspector.js
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import "firebase/firestore";
+import { getUsersLean } from "./fetchUsers";
 import { timeTable } from "./times";
 export const getStep = async () => {
   const db = firebase.firestore();
@@ -26,3 +27,35 @@ export const getDate = async () => {
   const db = firebase.firestore();
   return (await db.collection("gameData").doc("worldTime").get()).data()?.date;
 };
+
+export const cleanAnswer = async () => {
+  const db = firebase.firestore();
+  const users = await getUsersLean();
+  return await Promise.all(
+    users.map(({ id }) => {
+      return Promise.all([
+        db
+          .collection("users")
+          .doc(id)
+          .collection("gameMetaData")
+          .doc("quest")
+          .delete(),
+        db
+          .collection("users")
+          .doc(id)
+          .collection("gameMetaData")
+          .doc("quest_answers")
+          .delete(),
+        db
+          .collection("users")
+          .doc(id)
+          .collection("gameMetaData")
+          .doc("answers")
+          .delete(),
+      ]);
+    })
+  );
+  //https://us-central1-jstp-23-0.cloudfunctions.net/world-data-watch
+};
+
+window.cleanAnswer = cleanAnswer;
--- frontend/src/pages/intro/introVid.js
+++ frontend/src/pages/intro/introVid.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BaseLink, useRoute } from "react-router5";
+import React, { useState, useEffect } from "react";
+import { useRoute } from "react-router5";
 import styled from "styled-components";
 const DarkBG = styled.div`
   height: 100vh;
@@ -8,6 +8,146 @@ const DarkBG = styled.div`
 `;
 export default (props) => {
   const { router } = useRoute();
-
-  return <DarkBG>TikTok Liked</DarkBG>;
+  const [currentVid, setCurrentVid] = useState(0);
+  const Vids = [
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/BZtx1VTdqU8?autoplay=1"
+          frameborder="0"
+          title="1"></iframe>
+      ),
+      time: 46000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/hTPkaChql4k"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="2"></iframe>
+      ),
+      time: 39000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/7NLL94YDR8c"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="3"></iframe>
+      ),
+      time: 60200,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/ygYGigck_Hw"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="4"></iframe>
+      ),
+      time: 74000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/3rw7URwC8kU"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="5"></iframe>
+      ),
+      time: 55000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/_EF1Ycydkp8"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="6"></iframe>
+      ),
+      time: 60800,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/TaLCNdOZ9j0"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="7"></iframe>
+      ),
+      time: 60800,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/y08CIDgCx84"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="8"></iframe>
+      ),
+      time: 56000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/E1AaltDxrjM"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="9"></iframe>
+      ),
+      time: 54000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/1zK9ZrtIiyk"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="10"></iframe>
+      ),
+      time: 55000,
+    },
+    {
+      vid: (
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/jw_RWbLlPu4"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="11"></iframe>
+      ),
+      time: 49000,
+    },
+  ];
+  useEffect(() => {
+    if (currentVid <= 10) {
+      setTimeout(() => {
+        setCurrentVid(currentVid + 1);
+      }, Vids[currentVid].time);
+    }
+  }, [currentVid, Vids]);
+  return <DarkBG>{Vids[currentVid].vid}</DarkBG>;
 };
