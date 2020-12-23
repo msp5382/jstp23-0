68376aedbea85f3d490776e9ff43d28f4d964b90
matcha <meen.sankaew@gmail.com>
2020-12-23 08:28:55 +0700

    add more factor when decrese world data

--- functions/worldWatch/index.js
+++ functions/worldWatch/index.js
@@ -129,7 +129,7 @@ const calcConsq = (origin, action, time) => {
   }
 
   if (action === "-") {
-    return origin - timeFac;
+    return origin - timeFac * 2;
   } else if (action.includes("0")) {
     return origin;
   } else if (action === "+") {
